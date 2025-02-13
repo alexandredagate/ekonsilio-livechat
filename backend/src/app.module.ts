f973f3b45e39bfd9a7254efd66d79359e5fe4c6b
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-13 16:17:10 +0100

    ✨ create auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LiveChatDataSource } from '../Database';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(LiveChatDataSource),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
