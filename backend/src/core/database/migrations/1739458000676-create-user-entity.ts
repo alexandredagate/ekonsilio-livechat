a1e06573a5a1484c66593cdeede373c3c792e4de
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-13 15:51:08 +0100

    🗃️ user migration

---
+++ backend/src/core/database/migrations/1739458000676-create-user-entity.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUserEntity1739458000676 implements MigrationInterface {
+    name = 'CreateUserEntity1739458000676'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying(255) NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
