ee190c14009ef24e53a79d78f477cf1ba655e2e6
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-14 12:16:13 +0100

    ✨ add 'me' route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { Login as LoginType } from "../core/types/login.type";
+import { JwtAuthGuard } from "./jwt-auth.guard";
+import { Request as Req } from "express";
+import { User } from "../user/user.entity";
 
 @Controller("auth")
 export class AuthController {
@@ -13,5 +16,11 @@ export class AuthController {
   async Login(@Body() loginDto: LoginDto): Promise<LoginType> {
     const user = await this.authService.ValidateUser(loginDto.username, loginDto.password);
     return this.authService.Login(user);
+  }
+
+  @Get("me")
+  @UseGuards(JwtAuthGuard)
+  Me(@Request() req: Req): User {
+    return req.user! as User;
   }
 }
