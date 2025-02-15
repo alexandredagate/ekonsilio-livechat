586a5e868594abb850700008c2ac14fa380e6dc4
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-15 16:57:43 +0100

    ♻️ improve useConversations hook

--- frontend/admin/src/core/functions/hooks/useConversations.ts
+++ frontend/admin/src/core/functions/hooks/useConversations.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Conversation } from "../../types/Conversation";
 import { Request } from "../Request";
 import { notifications } from "@mantine/notifications";
+import { Message } from "../../types/Message";
 
 export function useConversations() {
   const [conversations, setConversations] = useState<Conversation[]>([]);
@@ -18,7 +19,19 @@ export function useConversations() {
       });
   }
 
+  function pushMessageIntoConversation(message: Message, conversationId: string) {
+    setConversations(o => {
+      const conv = o.find(i => i.id === conversationId);
+
+      if (!conv) return o;
+
+      conv.messages.push(message);
+
+      return [conv, ...o.filter(i => i.id !== conv.id)];
+    })
+  }
+
   useEffect(fetchData, []);
 
-  return { conversations };
+  return { conversations, pushMessageIntoConversation };
 }
