3821023e880e8a5b69a6760bb1afb81ed63e85f7
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-15 16:58:02 +0100

    ♻️ add conversation field to Message type

--- frontend/admin/src/core/types/Message.ts
+++ frontend/admin/src/core/types/Message.ts
@@ -1,8 +1,10 @@
+import { Conversation } from "./Conversation";
 import { User } from "./User";
 
 export type Message = {
   id: string;
   text: string;
+  conversation: Conversation;
   createdAt: Date;
   updatedAt: Date;
   operator?: User;
