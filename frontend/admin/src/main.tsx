126f9244a63aa709252a04a11f91d9d0954f6232
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-15 16:56:46 +0100

    ♻️ disable StrictMode to prevent weird behavior

--- frontend/admin/src/main.tsx
+++ frontend/admin/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MantineProvider } from '@mantine/core';
 import App from './App.tsx'
@@ -9,10 +8,8 @@ import '@mantine/notifications/styles.css';
 import { Notifications } from '@mantine/notifications';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <MantineProvider>
       <Notifications />
       <App />
-    </MantineProvider>
-  </StrictMode>,
+    </MantineProvider>,
 )
