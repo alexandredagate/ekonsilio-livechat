bb826ffa7dd8fd9fbfa8b839c5ca5a921fb6f7c9
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-16 18:22:00 +0100

    ♻️ replace conversation to conversationId

--- frontend/admin/src/core/types/Message.ts
+++ frontend/admin/src/core/types/Message.ts
@@ -1,10 +1,9 @@
-import { Conversation } from "./Conversation";
 import { User } from "./User";
 
 export type Message = {
   id: string;
   text: string;
-  conversation: Conversation;
+  conversationId: string;
   createdAt: Date;
   updatedAt: Date;
   operator?: User;
