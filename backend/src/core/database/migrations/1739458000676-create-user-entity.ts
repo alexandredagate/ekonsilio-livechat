222fc921336351a2faac32ca3086f72d040d3e90
Alexandre D'agate <dagate.alexandre@gmail.com>
2025-02-17 15:36:42 +0100

    🔨 add one more user on db migration run

--- backend/src/core/database/migrations/1739458000676-create-user-entity.ts
+++ backend/src/core/database/migrations/1739458000676-create-user-entity.ts
@@ -8,6 +8,7 @@ export class CreateUserEntity1739458000676 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "user" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username")`);
+        await queryRunner.query(`INSERT INTO "user" (username, password) VALUES ('genius0213', '$2a$12$j7A9pVnqd/fc8RdS5XojP.Gg03Ou9Oe98gXsWqOVP1qORbg2W1K6q')`);
         await queryRunner.query(`INSERT INTO "user" (username, password) VALUES ('genius0212', '$2a$12$j7A9pVnqd/fc8RdS5XojP.Gg03Ou9Oe98gXsWqOVP1qORbg2W1K6q')`);
     }
 
